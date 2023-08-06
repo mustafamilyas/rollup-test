792109b05c5de2bef9216b0847246d73e6bffe7d
Muhamad Ilyas Mustafa <emailpribadiilyas@gmail.com>
2023-08-06 21:49:11 +0700

    multiple output format

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,19 @@
 // rollup.config.mjs
 import json from "@rollup/plugin-json";
 import terser from "@rollup/plugin-terser";
+import pkg from "./package.json" assert { type: "json" };
 
 export default {
   input: "src/main.js",
-  output: {
-    file: "dist/bundle.js",
-    format: "cjs",
-  },
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      file: pkg.module,
+      format: "es",
+    },
+  ],
   plugins: [json(), terser()],
 };
